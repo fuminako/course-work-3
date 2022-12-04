d547de6f48643d8942421336598f308c7435fbc2
Анастасия <fumi_nako@mail.ru>
2022-12-04 06:56:15 +0300

    Initial commit

---
+++ src/main/java/com/skypro/coursework/CourseWork3Application.java
@@ -0,0 +1,13 @@
+package com.skypro.coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork3Application.class, args);
+    }
+
+}
